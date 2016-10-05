5f13258a4a83349d327c0df5899a8e7b3bfaf47d
ras <ras@z-ip.eu>
2016-10-05 21:40:04 +0200

    slug fix

---
+++ robot1.py
@@ -0,0 +1,28 @@
+import rg
+import math
+
+
+class Robot:
+
+    def sigmoid(x):
+        return 1 / (1 + math.exp(-x))
+
+    def act(self, game):
+        # if we're in the center, stay put
+        if self.location == rg.CENTER_POINT:
+            return ['guard']
+
+        # if there are enemies around, attack them
+        num = 0
+        for loc, bot in game.robots.iteritems():
+            if bot.player_id != self.player_id:
+                if rg.dist(loc, self.location) <= 1:
+                    num = num+1
+                    if num >3:
+                        return['suicide']
+                    else:
+                        return ['attack', loc]
+
+        # move toward the center
+        return ['move', rg.toward(self.location, rg.CENTER_POINT)]
+        pass
